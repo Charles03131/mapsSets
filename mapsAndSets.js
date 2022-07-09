988858886d42ddabf7b12f3dc0c7216781157195
Charles03131 <97250801+Charles03131@users.noreply.github.com>
2022-07-09 14:46:12 -0500

    Add files via upload

---
+++ mapsAndSets.js
@@ -0,0 +1,34 @@
+
+
+quick ANSWER 1
+
+{1,2,3,4}
+
+
+quick ANSWER 2
+
+"ref"
+
+
+quick ANSWER 3
+
+0: {Array(3) => true}
+1: {Array(3) => false}
+
+
+
+HAS DUPLICATE
+
+const hasDuplicate=arr=>{ return new Set(arr).size!==arr.length}
+
+
+VOWEL COUNT 
+
+
+I am not sure what the answer is for this one. I tried to work through the answer provided 
+but it returns
+
+//vowelCount('colt');
+
+// [object Map] 
+//{}
